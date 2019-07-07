a5d7c1b1060fa4b2b525afd395c24d1d50bf5ab3
Monika Famielec <Monika Famielec>
2019-07-07 14:45:30 +0200

    nedziela

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-cons user = {};
+let idUser = ''
+let mainDiv = document.createElement("div");
+function getIdUser() {
+    let idUser = document.getElementsByClassName('idUser')[0].value
 
-fetch('url')
-.then(res => res.json())
-.then(res =>{
-    this.user = res
-    wyswietlanie(this.user)
-})
+    console.log(idUser)
+
+    if (idUser > 0 && idUser <= 10) {
+        fetch('http://jsonplaceholder.typicode.com/users/' + idUser)
+            .then(res => res.json())
+            .then(res => {
+            console.log(res)
+            
+                mainDiv.innerHTML = ''
+                let paraEmail = document.createElement ('p')
+                let paraName = document.createElement ('p')
+                paraEmail.innerHTML = "<p id='mainID1'>" + res.address.zipcode + "</p>"
+                paraName.innerHTML = "<p id='mainID2'>" + res.name + "</p>"
+                mainDiv.appendChild(paraEmail)
+                mainDiv.appendChild(paraName)
+                document.body.appendChild(mainDiv)   
+                  
+        })  
+        
+        
+    } else {
+        alert("podana liczba " + idUser + " jest nieprawidłowa")
+
+    }
 
 
-function wyswietlanie (data) {}
+}
+
